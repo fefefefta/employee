813baf95df55ac86a344e7fd111d4ddec7baf2d5
Leo <guzeev.leonid.dc@gmail.com>
2022-09-26 19:40:57 +0300

    shop models

--- django_app/apps/shop/models.py
+++ django_app/apps/shop/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+
+class Tag(models.Model):
+    name = models.TextField()
+
+
+class Category(models.Model):
+    name = models.TextField()
+
+
+class Product(models.Model):
+    tags = models.ManyToManyField(Tag)
+    categories = models.ManyToManyField(Category)
+    name = models.TextField()
+    description = models.TextField()
+    price = models.TextField()
+    status = models.TextField()
+
+
+class Image(models.Model):
+    path = models.TextField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    status = models.TextField()
