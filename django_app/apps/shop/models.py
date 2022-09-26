394fb308050e843f3df176c21bd3b8aeeeb24584
Leo <guzeev.leonid.dc@gmail.com>
2022-09-26 20:04:25 +0300

    fix

--- django_app/apps/shop/models.py
+++ django_app/apps/shop/models.py
@@ -4,24 +4,24 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.TextField()
+    name = models.CharField()
 
 
 class Category(models.Model):
-    name = models.TextField()
+    name = models.CharField()
 
 
 class Product(models.Model):
     tags = models.ManyToManyField(Tag)
     categories = models.ManyToManyField(Category)
-    name = models.TextField()
+    name = models.CharField()
     description = models.TextField()
-    price = models.TextField()
-    status = models.TextField()
+    price = models.DecimalField()
+    status = models.BooleanField(default=True)
 
 
 class Image(models.Model):
-    path = models.TextField()
+    path = models.CharField()
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 
@@ -29,4 +29,4 @@ class Comment(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
-    status = models.TextField()
+    status = models.BooleanField(default=True)
