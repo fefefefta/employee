a134eceba6b69a0137c3c15bbf09d6c3e72729da
Leo <guzeev.leonid.dc@gmail.com>
2022-09-26 20:13:11 +0300

    fix

--- django_app/apps/shop/models.py
+++ django_app/apps/shop/models.py
@@ -4,29 +4,39 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=15)
 
 
 class Category(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=50)
 
 
 class Product(models.Model):
+    name = models.CharField(max_length=50)
     tags = models.ManyToManyField(Tag)
     categories = models.ManyToManyField(Category)
-    name = models.CharField()
     description = models.TextField()
-    price = models.DecimalField()
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     status = models.BooleanField(default=True)
 
 
 class Image(models.Model):
-    path = models.CharField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="product/image/")
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
 
 
 class Comment(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
     text = models.TextField()
     status = models.BooleanField(default=True)
